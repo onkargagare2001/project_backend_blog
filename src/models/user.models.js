34a7d408edf0141a58ba94d047318dcf518e618f
onkargagare2001 <onkargagare17@gmail.com>
2026-01-16 19:25:05 +0530

    password hasing with bcrypt , JWT token generation methods written in user model done

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,7 @@
 import mongoose,{Schema} from "mongoose";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
 
 const userSchema=new Schema({
     username:{
@@ -38,5 +41,47 @@ const userSchema=new Schema({
     ]
 
 },{timestamps:true});
+
+userSchema.pre("save",async function(next){
+    if(!this.isModified("password")) return next(); 
+    this.password = await bcrypt.hash(this.password,10);
+    next();
+})
+
+userSchema.methods.isPasswordCorrect=async function(password){
+   return await bcrypt.compare(password,this.password);
+}
+
+userSchema.methods.generateAccessToken=async function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+
+        },
+        process.env.ACCESS_TOKEN,
+        {
+            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken= function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+
+        },
+        process.env.REFRESH_TOKEN,
+        {
+            expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 
 export const User = mongoose.model('User',userSchema);
