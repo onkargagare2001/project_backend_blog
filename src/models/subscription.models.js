bc9e22cd0b4eaf0426f67c8484361bc1676bd272
Onkar Gagare <71821005+onkargagare2001@users.noreply.github.com>
2026-03-25 17:31:28 +0000

    subscription model is written and completed

---
+++ src/models/subscription.models.js
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from "mongoose";
+
+const subscriptionSchema = Schema(
+  {
+    channel: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    subscriber: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema);
