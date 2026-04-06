7c951ca26e02e734480665d468c66843b8247107
Onkar Gagare <71821005+onkargagare2001@users.noreply.github.com>
2026-04-06 17:33:12 +0000

    updateAvatar endpoint created

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -233,6 +233,36 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
   return res.status(200).json(new ApiResponse(200, {}, "Password changed."));
 });
 
+const updateAvatar = asyncHandler(async (req, res) => {
+  const avatarLocalPath = req.file?.path;
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "file dows not exist");
+  }
+
+  // we have to delete old image here before uploading new one which can optimize app
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+
+  if (!avatar.url) {
+    throw new ApiError(500, "Erro occurer while uploadOnCloudinary");
+  }
+
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $et: {
+        avatar: avatar.url,
+      },
+    },
+    {
+      new: true,
+    }
+  ).select("-password");
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user, "avatar image updated successfully"));
+});
+
 const updateAccountDetails = asyncHandler(async (req, res) => {
   const { fullname, email } = req.body;
   if (!fullname || !email) {
