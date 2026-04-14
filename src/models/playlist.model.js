94673e830d4c592a71b1f0af22b1c345502c8451
Onkar Gagare <71821005+onkargagare2001@users.noreply.github.com>
2026-04-14 06:27:40 +0000

    new models added like tweet, comment , playlist

---
+++ src/models/playlist.model.js
@@ -0,0 +1,26 @@
+import { Schema } from "mongoose";
+import mongoose from "mongoose";
+
+const playlistSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    videos: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Playlist = mongoose.model("Playlist", playlistSchema);
