025cc398eeeefe6eafd9a4296bd4da7d6bd58e1c
Onkar Gagare <71821005+onkargagare2001@users.noreply.github.com>
2026-05-13 06:28:05 +0000

    new endpoint added in susbcription controller

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -33,11 +33,57 @@ const toggleSubscription = asyncHandler(async (req, res) => {
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
   const { channelId } = req.params;
+  const subscribers = await Subscription.find({
+    channel: channelId,
+  });
+
+  if (!subscribers.length) {
+    throw new ApiError(400, "no susbcribers for this channel");
+  }
+
+  const count = subscribers.length;
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { subscribers, count },
+        count ? "Subscribers fechted" : "0 subscribers found"
+      )
+    );
 });
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
   const { subscriberId } = req.params;
+
+  const subscribedChannels = await Subscription.find({
+    subscriber: subscriberId,
+  })
+    .populate("channel", "username avatar")
+    .select("channel createdAt");
+
+  const channels = subscribedChannels.map((item) => ({
+    _id: item.channel._id,
+    username: item.channel.username,
+    avatar: item.channel.avatar,
+    createdAt: item.createdAt,
+  }));
+
+  const count = subscribedChannels.length;
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        channels,
+        count
+          ? "all subscribedChannels channels fetced"
+          : "no channel found subscriubers has subscribed"
+      )
+    );
 });
 
 export { toggleSubscription, getUserChannelSubscribers, getSubscribedChannels };
